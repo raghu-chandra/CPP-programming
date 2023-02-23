8f44689dfcee749f58726b654b88ae58e4dd4ab6
raghu-chandra <113702648+raghu-chandra@users.noreply.github.com>
2023-02-23 23:19:29 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int testcase;
+    cout<<"enter a num"<<endl;
+    cin>>testcase;
+    if(testcase%2==0){
+    cout<<"testcase is even"<<endl;
+    }else{
+        cout<<"testcase is odd"<<endl;
+    }
+    return 0;
+}
+
+
